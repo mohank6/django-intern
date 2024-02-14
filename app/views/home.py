8d1fec4eb7615d53f0fcbd396b716ad472d82246
Mohan <mohan@ramailo.tech>
2024-02-14 15:12:21 +0545

    add templates

--- app/views/home.py
+++ app/views/home.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from ..models import Post, Comment
 
 # from .. import fake_data
 
 
 def home(request):
     # fake_data.generate_fake_data()
-    return HttpResponse("Home")
+    posts = Post.objects.all()
+    return render(request, 'app/home.html', {'posts': posts})
